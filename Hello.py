49ef6eb6d62c23026850bff4c0b27029bf15898d
Ankit Singh <89828727+AnkitSin24@users.noreply.github.com>
2024-02-23 21:15:48 +0530

    added some more languages

--- Hello.py
+++ Hello.py
@@ -1,1 +1,79 @@
-print("hello") 
+Python and Swift:
+print("Hello, world!")
+
+
+Java:
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
+
+
+JavaScript:
+console.log("Hello, world!");
+
+
+C:
+#include <stdio.h>
+int main() {
+    printf("Hello, world!\n");
+    return 0;
+}
+
+
+C++:
+#include <iostream>
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
+
+
+Ruby:
+puts "Hello, world!"
+
+
+Go:
+package main
+import "fmt"
+func main() {
+    fmt.Println("Hello, world!")
+}
+
+
+C#:
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, world!");
+    }
+}
+
+
+PHP:
+<?php
+echo "Hello, world!";
+?>
+
+
+R:
+cat("Hello, world!\n")
+
+
+Kotlin
+fun main() {
+    println("Hello, world!")
+}
+
+Rust:
+fn main() {
+    println!("Hello, world!");
+}
+
+
+
+
+
